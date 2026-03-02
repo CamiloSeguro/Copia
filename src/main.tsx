fde2aece6fde4f684c0aea78f162731c6c1c2ee5
Camilo Seguro <82466584+CamiloSeguro@users.noreply.github.com>
2026-03-02 00:18:15 -0500

    Refactor CatalogProvider to use initialResources prop

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,21 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { AuthProvider } from "./auth/AuthContext";
 import { router } from "./app/routes";
-
 import { TicketProvider } from "./app/ticketContext";
 import { LoansProvider } from "./app/loansContext";
-
 import { CatalogProvider } from "./app/catalogContext";
 import { mockResources } from "./data/mockResources";
-
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <CatalogProvider seedResources={mockResources}>
-        <TicketProvider>
-          <LoansProvider>
-            <RouterProvider router={router} />
-          </LoansProvider>
-        </TicketProvider>
-      </CatalogProvider>
-    </AuthProvider>
-  </React.StrictMode>
+  <AuthProvider>
+    <CatalogProvider initialResources={mockResources}>
+      <TicketProvider>
+        <LoansProvider>
+          <RouterProvider router={router} />
+        </LoansProvider>
+      </TicketProvider>
+    </CatalogProvider>
+  </AuthProvider>
 );
